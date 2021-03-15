6d820e15894a8f980819d27e9b4ff6bf9fc3158f
Antoine Richard <antoine.richard03@edu.univ-fcomte.fr>
2021-03-15 13:05:08 +0100

    Modify group_item.xml with layout_rounded_corner.xml

--- app/src/main/java/com/project/taskmanager/JsonManager.java
+++ app/src/main/java/com/project/taskmanager/JsonManager.java
@@ -5,6 +5,24 @@ public class JsonManager {
     private String JsonPath = "";
 
     private void openJson(){
+        /*try
+        {
+
+        } catch ()
+        {
+
+        }*/
+    }
+
+    public void addGroupInJson(String groupName) {
+
+    }
+
+    public void addTaskInGroupInJson(String groupName, String taskName) {
+
+    }
+
+    public void addSubTaskInTaskInJson(String groupName, String taskName, String subTaskName) {
 
     }
 

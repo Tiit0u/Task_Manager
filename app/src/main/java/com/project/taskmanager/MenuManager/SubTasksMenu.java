f34bfcf8802d38407f8d2bd535ad7eb04ce207e0
Antoine Richard <antoine.richard03@edu.univ-fcomte.fr>
2021-03-18 19:58:21 +0100

    Add SubTasksMenu.java

---
+++ app/src/main/java/com/project/taskmanager/MenuManager/SubTasksMenu.java
@@ -0,0 +1,17 @@
+package com.project.taskmanager.MenuManager;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.taskmanager.R;
+
+public class SubTasksMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
